941dd5cbdf20ae61990070ebd524ddffe98ea5af
paulsolari <paulsolari@outlook.com>
2021-05-29 23:48:59 +0200

    z

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import "../styles/main.scss";
 import "./utils/resize.js";
 import {Player} from "./components/player.js";
 
+
 document.addEventListener("DOMContentLoaded", () => {
 	let player = new Player();
 });
